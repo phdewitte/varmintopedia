a74f3e99d5462d78c5e30f45fc2f75590abe0024
SJEble <sjeble@gmail.com>
2016-06-15 18:05:42 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# 10.times do |i|
-#   User.create!(email)
-# end
+10.times do |i|
+  User.create!(email: Faker::Internet.email, password: Faker::Internet.password)
+end
+
+User.all.each do |user|
+  article = Article.create!(author: user)
+  Version.create!(title: Faker::Superhero.name, body: Faker::Hipster.paragraph(4), editor: user, article: article)
+end
