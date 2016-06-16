75f918db83ac9856cc13742b35f91347afd0923f
SJEble <sjeble@gmail.com>
2016-06-16 14:58:42 -0500

    added in theron version methods

--- app/models/version.rb
+++ app/models/version.rb
@@ -10,6 +10,29 @@ class Version < ActiveRecord::Base
   after_save :assign_categories
 
 
+  def add_ids_to_h1s    
+   sections = []   
+   body_html = Nokogiri::HTML(self.body)   
+   h1s = body_html.css('h1')   
+   i = 0   
+   while i < h1s.length do   
+     h1s[i]['id'] = i    
+     sections << "<a href='##{i}'>#{h1s[i].text}</a>"    
+     i += 1    
+   end   
+   return sections   
+  end
+
+  def self.search(criteria)
+    if criteria
+      where('title LIKE ?', "%#{criteria}%")
+      where('body LIKE ?', "%#{criteria}%")
+    else
+      where(true)
+    end
+  end
+
+
   private
   
   def assign_categories
