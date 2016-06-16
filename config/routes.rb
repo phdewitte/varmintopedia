77c317db6a707de60c95caacc379063a685af07a
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-06-16 17:26:21 -0500

    Complete revision history listing

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ Rails.application.routes.draw do
   get '/users/:id' => 'users#show', as: :user
   get '/users' => 'users#index', as: :users
 
+  get '/versions/search' => 'versions#search'
+
+  get '/articles/:article_id/versions' => 'versions#index', as: :article_versions
+
+  get '/version/:id' => 'versions#show', as: :version
+
 end
