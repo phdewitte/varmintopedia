5d4d939388b5a8cfcc2253347adedffa2d514062
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-06-17 14:32:26 -0500

    delete extra migration

--- db/migrate/20160617191907_add_fields_to_version.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsToVersion < ActiveRecord::Migration
-  def change
-    add_column :versions, :published, :boolean, default: false
-    add_column :versions, :has_bibliography, :boolean, default: false
-  end
-end
